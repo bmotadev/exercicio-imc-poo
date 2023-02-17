313297d5b7b207d7dcab8f4af72bb9def4aff49a
Bruno Mota <bmotadev@gmail.com>
2023-02-17 01:13:25 -0300

    :sparkles: feat: Informações recebidas - informações para nome, idade, altura e peso

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:exercicio_imc_poo/src/pessoa.dart';
+import 'dart:io';
 
 Pessoa pessoa = Pessoa();
 void main(List<String> args) {
+  print("=== Escreva seu nome:");
+  pessoa.nome = stdin.readLineSync()!;
+
+  print("=== Escreva sua idade");
+  pessoa.idade = int.parse(stdin.readLineSync()!);
+
+  print("=== Escreva sua altura");
+  pessoa.altura = double.parse(stdin.readLineSync()!);
+
+  print("=== Escreva seu peso");
+  pessoa.peso = double.parse(stdin.readLineSync()!);
 }
